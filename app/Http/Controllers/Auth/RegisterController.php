cc3ca631c1628a4d561d084800b455c116c072c4
derek-5 <wjn123562077@gmail.com>
2019-10-18 15:35:22 -0400

    register verfication completed

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,8 +51,10 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-        ]);
+            'password' => ['required', 'string', 'min:6', 'confirmed'],
+            'captcha' => ['required', 'captcha']
+        ],['captcha.required' => 'Verification code may not be empty.',
+            'captcha.captcha' => 'Please enter correct verification code.']);
     }
 
     /**
