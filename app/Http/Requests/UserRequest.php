46951a587a020b6404d7323df0bcee7806369744
derek-5 <wjn123562077@gmail.com>
2019-10-22 14:15:31 -0400

    user edit

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+                'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
+                'email' => 'required|email',
+                'introduction' => 'max:80',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.unique' => 'User name has been used. Please enter another one.',
+            'name.regex' => 'User name supports english, number, dash and underscore.',
+            'name.between' => 'User name should be between 3 and 25 letters.',
+            'name.required' => 'User name can not be empty.',
+        ];
+    }
+}
---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+  <div class="col-md-8 offset-md-2">
+
+    <div class="card">
+      <div class="card-header">
+        <h4>
+          <i class="glyphicon glyphicon-edit"></i> Edit Personal Info
+        </h4>
+      </div>
+
+      <div class="card-body">
+
+        <form action="{{ route('users.update', $user->id) }}" method="POST" accept-charset="UTF-8">
+          <input type="hidden" name="_token" value="{{ csrf_token() }}">
+          <input type="hidden" name="_method" value="PUT">
+          @include('shared._error')
+          <div class="form-group">
+            <label for="name-field">User Name</label>
+            <input class="form-control" type="text" name="name" id="name-field" value="{{ old('name', $user->name) }}" />
+          </div>
+          <div class="form-group">
+            <label for="email-field">Email</label>
+            <input class="form-control" type="text" name="email" id="email-field" value="{{ old('email', $user->email) }}" />
+          </div>
+          <div class="form-group">
+            <label for="introduction-field">Personal Info</label>
+            <textarea name="introduction" id="introduction-field" class="form-control" rows="3">{{ old('introduction', $user->introduction) }}</textarea>
+          </div>
+          <div class="well well-sm">
+            <button type="submit" class="btn btn-primary">Save</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+@stop
