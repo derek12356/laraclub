75ad025974b85062e57bdbf9048e61d311ff9314
derek-5 <wjn123562077@gmail.com>
2020-02-04 17:27:23 -0500

    setup email

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'derekwu@bimmerbook.com'),
+        'name' => env('MAIL_FROM_NAME', 'bimmerbook.com'),
     ],
 
     /*
